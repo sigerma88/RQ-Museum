163f1af11b233a99dff791dc2785978569098f4d
Victor <victor.j.zhao@gmail.com>
2022-10-22 18:57:51 -0400

    issue33 added comments to manager

--- Museum-Backend/src/main/java/ca/mcgill/ecse321/museum/dao/ManagerRepository.java
+++ Museum-Backend/src/main/java/ca/mcgill/ecse321/museum/dao/ManagerRepository.java
@@ -2,6 +2,10 @@ package ca.mcgill.ecse321.museum.dao;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.museum.model.Manager;
 
+/**
+ * Author : VZ
+ * This is the repository for the Manager class
+ */
 public interface ManagerRepository extends CrudRepository<Manager, Long>{
 
 	Manager findTicketByManagerId(Long managerId);
