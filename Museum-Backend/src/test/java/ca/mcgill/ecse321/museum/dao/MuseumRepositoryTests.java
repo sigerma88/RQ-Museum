57637212fbef05877168bacf4d78108fc8d36725
kieyanmamiche <kieyanmamiche@gmail.com>
2022-10-26 18:13:00 -0400

    Removed issue 54 because it was duplicate. Fixed up issues 50 and 59.

--- Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/MuseumRepositoryTests.java
+++ Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/MuseumRepositoryTests.java
@@ -1,14 +1,16 @@
 package ca.mcgill.ecse321.museum.dao;
 
+import ca.mcgill.ecse321.museum.model.Employee;
 import ca.mcgill.ecse321.museum.model.Museum;
 import ca.mcgill.ecse321.museum.model.Schedule;
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * Written by Kieyan
+ * @author Kieyan
  * Museum Repository test class
  * Here we test the museum repository interface by saving a loan into the database, querying for it,
  * and then checking if the results are consistent
@@ -17,37 +19,53 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MuseumRepositoryTests {
 
     @Autowired
-    private MuseumRepository museumRepository;
+    MuseumRepository museumRepository;
     @Autowired
-    private ScheduleRepository scheduleRepository;
+    ScheduleRepository scheduleRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @AfterEach
     public void clearDatabase() {
         museumRepository.deleteAll();
         scheduleRepository.deleteAll();
+        employeeRepository.deleteAll();
     }
 
     @Test
     public void testPersistAndLoadMuseum() {
+
         // Create objects
         Schedule schedule = new Schedule();
         Museum museum = new Museum();
+        Employee employee = new Employee();
+        employee.setName("Tim");
+        employee.setEmail("Tim@gmail.com");
+        employee.setPassword("123123");
         museum.setName("The Louvre");
-        museum.setSchedule(schedule);
         museum.setVisitFee(19.99);
-
+        employee.setSchedule(schedule);
+        schedule.setEmployee(employee);
+        schedule.setMuseum(museum);
+        museum.setSchedule(schedule);
 
         // Save object
+        employeeRepository.save(employee);
         scheduleRepository.save(schedule);
         museumRepository.save(museum);
         long id = museum.getMuseumId();
 
 
         // Read object from database
-        Museum museumRead = museumRepository.findMuseumByMuseumId(id);
+        Museum museumFromDB = museumRepository.findMuseumByMuseumId(id);
 
         // Assert that object has correct attributes
-        // ASSERT EQUALS IS NOT WORKING
+        Assert.assertEquals(museum.getName(), museumFromDB.getName()); // test name
+        Assert.assertEquals(museum.getMuseumId(), museumFromDB.getMuseumId()); // test id
+        Assert.assertEquals(museum.getSchedule().getScheduleId(), museumFromDB.getSchedule().getScheduleId()); // test schedule
+        Assert.assertEquals(museum.getSchedule().getEmployee().getMuseumUserId(), museumFromDB.getSchedule().getEmployee().getMuseumUserId()); // test employee linked to schedule
+        Assert.assertEquals(museum.getVisitFee(), museumFromDB.getVisitFee(),  0.001); // test visit fee
 
 
     }
--- Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/VisitorRepositoryTests.java
+++ Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/VisitorRepositoryTests.java
@@ -1,14 +1,14 @@
 package ca.mcgill.ecse321.museum.dao;
 
-import ca.mcgill.ecse321.museum.dao.VisitorRepository;
 import ca.mcgill.ecse321.museum.model.Visitor;
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * Written by Kieyan
+ * @author Kieyan
  * Visitor Repository test class
  * Here we test the visitor repository interface by saving a loan into the database, querying for it,
  * and then checking if the results are consistent
@@ -17,7 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class VisitorRepositoryTests {
 
     @Autowired
-    private VisitorRepository visitorRepository;
+    VisitorRepository visitorRepository;
 
     @AfterEach
     public void clearDatabase() {
@@ -40,11 +40,13 @@ public class VisitorRepositoryTests {
         long id = visitor.getMuseumUserId();
 
         // Read object from database
-        Visitor visitorRead = visitorRepository.findVisitorByMuseumUserId(id);
+        Visitor visitorFromDB = visitorRepository.findVisitorByMuseumUserId(id);
 
         // Assert that object has correct attributes
-        // ASSERT EQUALS IS NOT WORKING
-
+        Assert.assertEquals(visitor.getName(), visitorFromDB.getName()); // test name
+        Assert.assertEquals(visitor.getEmail(), visitorFromDB.getEmail()); // test name
+        Assert.assertEquals(visitor.getPassword(), visitorFromDB.getPassword()); // test name
+        Assert.assertEquals(visitor.getMuseumUserId(), visitorFromDB.getMuseumUserId()); // test name
 
     }
 
