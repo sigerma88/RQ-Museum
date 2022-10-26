5cb6b51259e3237a8362039490ad329aceba6b1d
Siger Ma <siger.ma@mail.mcgill.ca>
2022-10-26 15:51:18 -0400

    issue61 TimePeriod dao test

---
+++ Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/TimePeriodRepositoryTests.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.museum.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;  
+
+import ca.mcgill.ecse321.museum.model.TimePeriod;
+
+public class TimePeriodRepositoryTests {
+  
+}
