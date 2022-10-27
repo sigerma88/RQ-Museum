88a71f7c04e7432f093aef76a3cf7bd27b2a9993
Don-Barista <eric.joung@mail.mcgill.ca>
2022-10-27 11:14:45 -0400

    added EmployeeRepositoryTests and ManagerRepositoryTests

---
+++ Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/EmployeeRepositoryTests.java
@@ -0,0 +1,66 @@
+package ca.mcgill.ecse321.museum.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.museum.model.Employee;
+import ca.mcgill.ecse321.museum.model.Museum;
+import ca.mcgill.ecse321.museum.model.MuseumSystem;
+import ca.mcgill.ecse321.museum.model.Schedule;
+
+/** Test the persistence layer for the EmployeeRepository. Testing reading and writing of 
+ *  objects, attributes and references to the database.
+ *  @author Eric
+ */
+@SpringBootTest
+public class EmployeeRepositoryTests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        employeeRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistandLoadEmployee() {
+        
+        //create employee
+        Employee employee = new Employee();
+        employee.setName("Broseph");
+        employee.setEmail("aEmail");
+        employee.setPassword("aPassword");
+
+        //create schedule
+        Schedule schedule = new Schedule();
+
+        //associate employee to schedule
+        employee.setSchedule(schedule);
+
+        //save employee
+        employee = employeeRepository.save(employee);
+        long id = employee.getMuseumUserId();
+
+        //reset employee
+        employee = null;
+
+        //read employee from database
+        employee = employeeRepository.findEmployeeByMuseumUserId(id);
+
+        //assert that employee has correct attributes
+        assertNotNull(employee);
+        assertEquals(id, employee.getMuseumUserId());
+        assertEquals("Broseph", employee.getName());
+        assertEquals("aEmail", employee.getEmail());
+        assertEquals("aPassword", employee.getPassword());
+    }
+}
---
+++ Museum-Backend/src/test/java/ca/mcgill/ecse321/museum/dao/ManagerRepositoryTests.java
@@ -0,0 +1,58 @@
+package ca.mcgill.ecse321.museum.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.museum.model.Manager;
+import ca.mcgill.ecse321.museum.model.Museum;
+import ca.mcgill.ecse321.museum.model.MuseumSystem;
+import ca.mcgill.ecse321.museum.model.Schedule;
+
+
+/** Test the persistence layer for the ManagerRepository. Testing reading and writing of 
+ *  objects, attributes and references to the database.
+ *  @author Eric
+ */
+@SpringBootTest
+public class ManagerRepositoryTests {
+    @Autowired
+    private ManagerRepository managerRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        managerRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistandLoadManager() {
+        
+        //create manager 
+        Manager manager = new Manager();
+        manager.setName("Jeremy");
+        manager.setEmail("aEmail");
+        manager.setPassword("aPassword");
+
+        //save manager
+        manager = managerRepository.save(manager);
+        long id = manager.getMuseumUserId();
+
+        //reset manager
+        manager = null;
+
+        //read manager from database
+        manager = managerRepository.findManagerByMuseumUserId(id);
+
+        //assert that manager has correct attributes
+        assertNotNull(manager);
+        assertEquals(id, manager.getMuseumUserId());
+        assertEquals("Jeremy", manager.getName());
+        assertEquals("aEmail", manager.getEmail());
+        assertEquals("aPassword", manager.getPassword());
+    }
+}
