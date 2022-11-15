102068bf1ba05985529571a5777932ec3116f116
Victor <victor.j.zhao@gmail.com>
2022-11-15 13:02:09 -0500

    issue28-81 museumController works with postman

--- Museum-Backend/src/main/java/ca/mcgill/ecse321/museum/controller/MuseumRestController.java
+++ Museum-Backend/src/main/java/ca/mcgill/ecse321/museum/controller/MuseumRestController.java
@@ -1,5 +1,8 @@
 package ca.mcgill.ecse321.museum.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +10,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.museum.dto.MuseumDto;
-import ca.mcgill.ecse321.museum.dto.ScheduleDto;
+import ca.mcgill.ecse321.museum.model.Museum;
 import ca.mcgill.ecse321.museum.model.Schedule;
 import ca.mcgill.ecse321.museum.service.MuseumService;
 import ca.mcgill.ecse321.museum.service.ScheduleService;
@@ -44,17 +46,15 @@ public class MuseumRestController {
    * @author VZ
    * @param name
    * @param visitFee
-   * @param scheduleDto
    * @return
    */
   @PostMapping(value = { "/museum/app", "/museum/app/" })
   public ResponseEntity<?> createMuseum(
       @RequestParam(name = "name") String name,
-      @RequestParam(name = "visitFee") double visitFee,
-      @RequestBody ScheduleDto scheduleDto) {
+      @RequestParam(name = "visitFee") double visitFee) {
 
     try {
-      Schedule schedule = scheduleService.getSchedule(scheduleDto.getScheduleId());
+      Schedule schedule = scheduleService.createSchedule();
       return new ResponseEntity<>(DtoUtility.convertToDto(museumService.createMuseum(name, visitFee, schedule)),
           HttpStatus.OK);
     } catch (Exception e) {
@@ -63,23 +63,39 @@ public class MuseumRestController {
   }
 
   /**
-   * API to edit the museum
+   * API to edit the museum's name or visit fee
    * @param id
    * @param name
    * @param visitFee
-   * @param scheduleDto
    * @return
    */
-  @PostMapping(value = { "/museum/{id}/app", "/museum/{id}/app/" })
+  @PostMapping(value = { "/museum/app/edit/{id}/", "/museum/app/edit/{id}" })
   public ResponseEntity<?> editMuseum(
       @PathVariable("id") long id,
       @RequestParam(name = "name") String name,
-      @RequestParam(name = "visitFee") double visitFee,
-      @RequestBody ScheduleDto scheduleDto) {
+      @RequestParam(name = "visitFee") double visitFee) {
     try {
-      Schedule schedule = scheduleService.getSchedule(scheduleDto.getScheduleId());
+      Schedule schedule = museumService.getMuseum(id).getSchedule();
       return new ResponseEntity<>(DtoUtility.convertToDto(museumService.editMuseum(id, name, visitFee, schedule)),
           HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  /**
+   * API to get all museums
+   * @author VZ
+   * @return
+   */
+  @GetMapping(value = {"/museums", "/museums/"})
+  public ResponseEntity<?> getAllMuseums() {
+    try {
+      List<MuseumDto> museums = new ArrayList<>();
+      for (Museum museum : museumService.getAllMuseums()) {
+        museums.add(DtoUtility.convertToDto(museum));
+      }
+      return new ResponseEntity<>(museums, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
